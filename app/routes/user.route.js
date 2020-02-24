0eabe5de19acbd26acd7aa2440f7c8246040d26e
waqas740 <waqasiqbal740@gmail.com>
2020-02-25 02:57:24 +0500

    remove extra files

--- app/routes/user.route.js
+++
@@ -1,8 +0,0 @@
-const router = require('express').Router();
-const UserCtrl = require('C:/workplace/hub/app/controllers');
-router.get('/users/:id', UserCtrl.getUser);
-router.get('/users', UserCtrl.getUser);
-router.post('/users', UserCtrl.createUser);
-router.put('/users/:id', UserCtrl.updateUser);
-router.delete('/users/:id', UserCtrl.removeUser);
-module.exports = router;
