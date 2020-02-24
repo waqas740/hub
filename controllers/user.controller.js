e7e9dfa4a7412c7ba6ef7812d31706222ff81be7
waqas740 <waqasiqbal740@gmail.com>
2020-02-24 06:16:32 +0500

    .hub file creation

--- controllers/user.controller.js
+++
@@ -1,6 +0,0 @@
-'use strict';
-module.exports.get user = async (req, res, next) => {}
-module.exports.get user s = async (req, res, next) => {}
-module.exports.create user = async (req, res, next) => {}
-module.exports.remove user = async (req, res, next) => {}
-module.exports.update user = async (req, res, next) => {}
--- hub.js
+++ hub.js
@@ -1,21 +1,17 @@
-#!/usr/bin/env node
+
 const { getYArgs } = require('./lib/core/yargs')
 const yargs = getYArgs();
-
 const init = require('./lib/commands/init');
+const touch = require('./lib/commands/hub-file');
 const crud = require('./lib/commands/crud-generator');
-//import migrationGenerate from './commands/migration_generate';
-//import modelGenerate from './commands/model_generate';
 const helpers = require('./lib/helpers');
-
 helpers.view.teaser();
-
 const cli = yargs
     .help()
     .version()
-    .command('init', 'Initializes project', init)
-    .command("crud", 'Generates crud', crud)
-    //   .command(['model:generate', 'model:create'], 'Generates a model and its migration', modelGenerate)
+    .command('touch', 'create .hub file', touch)
+    .command('init', 'Initializes Folders', init)
+    .command("crud", 'Generates crud operation', crud)
     .wrap(yargs.terminalWidth())
     .strict();
 
